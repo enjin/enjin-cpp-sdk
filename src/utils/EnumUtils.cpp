864f369577c01d50cff5b009e0e2e27083105f6f
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-12-09 13:18:55 -0800

    - Added template generation scripts - Refactored templates into being a string mapping constants generated by scripts

--- src/utils/EnumUtils.cpp
+++ src/utils/EnumUtils.cpp
@@ -4,18 +4,6 @@ namespace enjin {
 namespace sdk {
 namespace utils {
 
-std::string get_name(enjin::sdk::graphql::TemplateType e) {
-    // No default case to produce compiler warning
-    switch (e) {
-        case enjin::sdk::graphql::FRAGMENT:
-            return "FRAGMENT";
-        case enjin::sdk::graphql::MUTATION:
-            return "MUTATION";
-        case enjin::sdk::graphql::QUERY:
-            return "QUERY";
-    }
-}
-
 } // namespace
 } // namespace
 } // namespace
