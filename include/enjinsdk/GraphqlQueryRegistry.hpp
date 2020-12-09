fa19e5bbda1615f8414383e101566bb62ef89699
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-12-09 13:37:04 -0800

    Added documentation to GraphqlQueryRegistry

--- include/enjinsdk/GraphqlQueryRegistry.hpp
+++ include/enjinsdk/GraphqlQueryRegistry.hpp
@@ -8,16 +8,29 @@ namespace enjin {
 namespace sdk {
 namespace graphql {
 
+/// \brief Class for registering and storing GraphQL templates.
 class GraphqlQueryRegistry {
 public:
+    /// \brief Default constructor.
     GraphqlQueryRegistry();
 
+    /// \brief Default destructor.
     ~GraphqlQueryRegistry();
 
+    /// \brief Registers a new template if one with the given name does not already exist.
+    /// \param name The template name.
+    /// \param query The template to register.
+    /// \return True if registered, false if a template with the given name already exists.
     bool register_template(const std::string &name, const std::string &query);
 
+    /// \brief Returns true if this registry contains a template with the given name.
+    /// \param name The template name.
+    /// \return True if the name is registered, false otherwise.
     bool has_operation_for_name(const std::string &name);
 
+    /// \brief Returns the template string if the registry contains the template or an empty string if not.
+    /// \param name The template name.
+    /// \return The template string.
     std::string get_operation_for_name(const std::string &name);
 
 private:
