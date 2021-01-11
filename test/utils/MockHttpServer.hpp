cd1f495de66029754f97ee5ea757fefb8f3e01eb
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-11 10:25:25 -0800

    Refactored MockHttpServer to map a given response for a expected request instead of simply enqueueing responses

--- test/utils/MockHttpServer.hpp
+++ test/utils/MockHttpServer.hpp
@@ -1,30 +1,45 @@
 #ifndef ENJINCPPSDK_MOCKHTTPSERVER_HPP
 #define ENJINCPPSDK_MOCKHTTPSERVER_HPP
 
+#include "enjinsdk/HttpRequest.hpp"
+#include "enjinsdk/HttpResponse.hpp"
 #include "cpprest/http_listener.h"
 #include "cpprest/http_msg.h"
-#include <vector>
+#include <map>
+#include <string>
 
 namespace enjin::test::utils {
 
+/// \brief Mock HTTP server built on Microsoft's C++ Rest SDK for testing HTTP client implementation.
 class MockHttpServer {
-    // TODO: Add method to map a stub-response for a given request.
 public:
+    /// \brief Constructs a mock server instance.
     MockHttpServer();
 
+    /// \brief Default destructor.
+    /// \remarks Does not shutdown the server.
     ~MockHttpServer() = default;
 
+    /// \brief Method to start the server.
     void start();
 
+    /// \brief Method to shutdown the server.
     void shutdown();
 
-    void enqueue(const web::http::http_response& response);
+    /// \brief Creating a mapping for an expected request and stubbed response.
+    /// \param request The request to expect.
+    /// \param response The response to reply with.
+    void map_response_for_request(const enjin::sdk::http::HttpRequest& request,
+                                  const enjin::sdk::http::HttpResponse& response);
 
-    web::http::uri uri(const std::string& path);
+    /// \brief Returns the URI for this server.
+    /// \param path Optional path to extend the URI by.
+    /// \return The URI.
+    web::http::uri uri(const std::string& path = "/");
 
 private:
     web::http::experimental::listener::http_listener listener;
-    std::vector<web::http::http_response> response_queue;
+    std::map<enjin::sdk::http::HttpRequest, enjin::sdk::http::HttpResponse> request_response_map;
 };
 
 }
