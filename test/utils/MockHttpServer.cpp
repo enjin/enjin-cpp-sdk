116b8e2d14d84537ca98c6b42cd5efb377f20134
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-11 10:25:25 -0800

    Refactored MockHttpServer to map a given response for a expected request instead of simply enqueueing responses

--- test/utils/MockHttpServer.cpp
+++ test/utils/MockHttpServer.cpp
@@ -2,25 +2,46 @@
 
 namespace enjin::test::utils {
 
+enjin::sdk::http::HttpRequest casablanca_to_enjin_request(web::http::http_request casablanca_req) {
+    return enjin::sdk::http::HttpRequestBuilder()
+            .method(utility::conversions::to_utf8string(casablanca_req.method()))
+            .path_query_fragment(utility::conversions::to_utf8string(casablanca_req.request_uri().to_string()))
+            .body(casablanca_req.extract_utf8string().get())
+            .content_type(utility::conversions::to_utf8string(casablanca_req.headers().content_type()))
+            .build();
+}
+
+web::http::http_response enjin_to_casablanca_response(enjin::sdk::http::HttpResponse enjin_res) {
+    web::http::http_response casablanca_res;
+
+    if (enjin_res.get_code().has_value()) {
+        casablanca_res.set_status_code(enjin_res.get_code().value());
+    }
+    if (enjin_res.get_body().has_value()) {
+        if (enjin_res.get_content_type().has_value() && !enjin_res.get_content_type().value().empty()) {
+            casablanca_res.set_body(enjin_res.get_body().value(),
+                                    enjin_res.get_content_type().value());
+        } else {
+            casablanca_res.set_body(enjin_res.get_body().value());
+        }
+    }
+
+    return casablanca_res;
+}
+
 MockHttpServer::MockHttpServer() {
     // TODO: Make the port # dynamic if necessary.
-    /*
-    web::uri_builder builder;
-    builder.set_scheme(U("http"))
-           .set_host(U("127.0.0.1"))
-           .set_port(8080);*/
-
     listener = web::http::experimental::listener::http_listener(U("http://localhost:8080/"));
 
     // Sets the listener to support any request by replying with the response in the front of the queue
     listener.support([this](const web::http::http_request& request) {
-        if (this->response_queue.empty()) {
-            // TODO: Change handling of empty response queue if appropriate.
-            request.reply(web::http::status_codes::TooManyRequests);
+        auto mapped_response_loc = request_response_map.find(casablanca_to_enjin_request(request));
+
+        if (mapped_response_loc != request_response_map.end()) {
+            enjin::sdk::http::HttpResponse response = mapped_response_loc->second;
+            request.reply(enjin_to_casablanca_response(response));
         } else {
-            auto& response = this->response_queue.front();
-            this->response_queue.erase(this->response_queue.begin());
-            request.reply(response);
+            request.reply(web::http::status_codes::TooManyRequests);
         }
     });
 }
@@ -33,8 +54,9 @@ void MockHttpServer::shutdown() {
     listener.close();
 }
 
-void MockHttpServer::enqueue(const web::http::http_response& response) {
-    response_queue.push_back(response);
+void MockHttpServer::map_response_for_request(const enjin::sdk::http::HttpRequest& request,
+                                              const enjin::sdk::http::HttpResponse& response) {
+    request_response_map.try_emplace(request, response);
 }
 
 web::http::uri MockHttpServer::uri(const std::string& path) {
