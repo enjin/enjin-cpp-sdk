0e1a01fbfe335124e89559fb13f4c4e23db4f198
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-11 10:23:17 -0800

    - Added HttpResponseBuilder - Refactored HttpResponse to be instantiated only by its builder - Added content type string member to HttpResponse - Added equality operators to HttpResponse

--- include/enjinsdk/HttpResponse.hpp
+++ include/enjinsdk/HttpResponse.hpp
@@ -6,25 +6,85 @@
 
 namespace enjin::sdk::http {
 
-class HttpResponse {
+class HttpResponse;
+
+/// \brief Builder class for HttpResponse class.
+class HttpResponseBuilder {
 public:
-    HttpResponse() = default;
+    /// \brief Default constructor.
+    HttpResponseBuilder() = default;
 
-    HttpResponse(uint16_t code, const std::string& body);
+    /// \brief Default destructor.
+    ~HttpResponseBuilder() = default;
 
-    ~HttpResponse() = default;
+    /// \brief Builds the response.
+    /// \return The built response.
+    HttpResponse build();
 
-    bool is_success();
+    /// \brief Sets the status code the response will be built with.
+    /// \param code The status code.
+    /// \return This builder for chaining.
+    HttpResponseBuilder& code(unsigned short code);
 
-    bool is_empty();
+    /// \brief Sets the body data the response will be built with.
+    /// \param body The response body.
+    /// \return This builder for chaining.
+    HttpResponseBuilder& body(const std::string& body);
 
-    std::optional<uint16_t> get_code();
-
-    std::optional<std::string> get_body();
+    /// \brief Sets the content type header the response will be built with.
+    /// \param content_type The content type.
+    /// \return This builder for chaining.
+    HttpResponseBuilder& content_type(const std::string& content_type);
 
 private:
-    std::optional<uint16_t> code;
+    std::optional<unsigned short> m_code;
+    std::optional<std::string> m_body;
+    std::optional<std::string> m_content_type;
+};
+
+/// \brief Container class for a HTTP response.
+class HttpResponse {
+public:
+    /// \brief Default constructor.
+    HttpResponse() = default;
+
+    /// \brief Default destructor.
+    ~HttpResponse() = default;
+
+    /// \brief True if the status code is in the range [200-300).
+    /// \return True if 2xx, false otherwise.
+    bool is_success();
+
+    /// \brief Determines if the response body is empty.
+    /// \return Whether the response body is empty.
+    bool is_empty();
+
+    /// \brief Returns the optional with the status code.
+    /// \return The status code optional.
+    [[nodiscard]] std::optional<unsigned short> get_code();
+
+    /// \brief Returns the optional with the response body.
+    /// \return The response body optional.
+    [[nodiscard]] std::optional<std::string> get_body();
+
+    /// \brief Returns the optional with the content type header.
+    /// \return The content type optional.
+    [[nodiscard]] std::optional<std::string> get_content_type();
+
+    bool operator==(const HttpResponse& rhs) const;
+
+    bool operator!=(const HttpResponse& rhs) const;
+
+private:
+    std::optional<unsigned short> code;
     std::optional<std::string> body;
+    std::optional<std::string> content_type;
+
+    HttpResponse(std::optional<unsigned short> code,
+                 std::optional<std::string>,
+                 std::optional<std::string> content_type);
+
+    friend HttpResponse HttpResponseBuilder::build();
 };
 
 }
