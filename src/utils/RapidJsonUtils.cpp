1fbd0cb9c6613596d5b37db68e4c64de751beece
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-14 15:45:35 -0800

    Added member function to RapidJsonUtils to get vector of strings from serialized array

--- src/utils/RapidJsonUtils.cpp
+++ src/utils/RapidJsonUtils.cpp
@@ -5,6 +5,30 @@
 
 namespace enjin::sdk::utils {
 
+std::vector<std::string> get_array_as_serialized_vector(const rapidjson::Document& document, const std::string& key) {
+    if (key.empty() && document.IsArray()) {
+        std::vector<std::string> v;
+        for (auto& arr_el : document.GetArray()) {
+            rapidjson::StringBuffer buffer;
+            rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+            arr_el.Accept(writer);
+            v.emplace_back(buffer.GetString());
+        }
+        return v;
+    } else if (!key.empty() && document[key.c_str()].IsArray()) {
+        std::vector<std::string> v;
+        for (auto& arr_el : document[key.c_str()].GetArray()) {
+            rapidjson::StringBuffer buffer;
+            rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+            arr_el.Accept(writer);
+            v.emplace_back(buffer.GetString());
+        }
+        return v;
+    }
+
+    throw std::exception("JSON element is not an array");
+}
+
 std::string get_object_as_string(const rapidjson::Document& document, const std::string& key) {
     if (key.empty() && document.IsObject()) {
         rapidjson::StringBuffer buffer;
