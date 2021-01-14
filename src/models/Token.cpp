22d1ab6f6bf377c6c9e9375195934a05bbf1cf2e
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-13 17:00:13 -0800

    - Added equality operators for Token - Implemented deserialization member function for Token

--- src/models/Token.cpp
+++ src/models/Token.cpp
@@ -1,5 +1,9 @@
 #include "enjinsdk/models/Token.hpp"
-#include "rapidjson/document.h"
+
+#include "EnumUtils.hpp"
+#include "RapidJsonUtils.hpp"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 namespace enjin::sdk::models {
 
@@ -7,7 +11,44 @@ void Token::deserialize(const std::string& json) {
     rapidjson::Document document;
     document.Parse(json.c_str());
     if (document.IsObject()) {
+        if (document.HasMember(ID_KEY) && document[ID_KEY].IsString()) {
+            id.emplace(document[ID_KEY].GetString());
+        }
+        if (document.HasMember(NAME_KEY) && document[NAME_KEY].IsString()) {
+            name.emplace(document[NAME_KEY].GetString());
+        }
+        if (document.HasMember(STATE_DATA_KEY) && document[STATE_DATA_KEY].IsObject()) {
+            TokenStateData v;
+            v.deserialize(utils::get_object_as_string(document, STATE_DATA_KEY));
+            state_data.emplace(v);
+        }
+        if (document.HasMember(CONFIG_DATA_KEY) && document[CONFIG_DATA_KEY].IsObject()) {
+            TokenConfigData v;
+            v.deserialize(utils::get_object_as_string(document, CONFIG_DATA_KEY));
+            config_data.emplace(v);
+        }
+        if (document.HasMember(VARIANT_MODE_KEY) && document[VARIANT_MODE_KEY].IsString()) {
+            variant_mode.emplace(utils::deserialize_token_variant_mode(document[VARIANT_MODE_KEY].GetString()));
+        }
+        if (document.HasMember(VARIANTS_KEY) && document[VARIANTS_KEY].IsArray()) {
+            std::vector<TokenVariant> v;
+            for (auto& arr_el : document[VARIANTS_KEY].GetArray()) {
+                TokenVariant v_el;
+                rapidjson::StringBuffer buffer;
+                rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+                arr_el.Accept(writer);
 
+                v_el.deserialize(buffer.GetString());
+                v.push_back(v_el);
+            }
+            variants.emplace(v);
+        }
+        if (document.HasMember(CREATED_AT_KEY) && document[CREATED_AT_KEY].IsString()) {
+            created_at.emplace(document[CREATED_AT_KEY].GetString());
+        }
+        if (document.HasMember(UPDATED_AT_KEY) && document[UPDATED_AT_KEY].IsString()) {
+            updated_at.emplace(document[UPDATED_AT_KEY].GetString());
+        }
     }
 }
 
@@ -41,6 +82,21 @@ const std::optional<std::string>& Token::get_created_at() const {
 
 const std::optional<std::string>& Token::get_updated_at() const {
     return updated_at;
+}
+
+bool Token::operator==(const Token& rhs) const {
+    return id == rhs.id &&
+           name == rhs.name &&
+           state_data == rhs.state_data &&
+           config_data == rhs.config_data &&
+           variant_mode == rhs.variant_mode &&
+           variants == rhs.variants &&
+           created_at == rhs.created_at &&
+           updated_at == rhs.updated_at;
+}
+
+bool Token::operator!=(const Token& rhs) const {
+    return !(rhs == *this);
 }
 
 }
--- test/unit/models/TokenTest.cpp
+++ test/unit/models/TokenTest.cpp
@@ -1,18 +1,139 @@
+#include "ModelsTestSuite.hpp"
 #include "enjinsdk/models/Token.hpp"
-#include "gtest/gtest.h"
 
 using namespace enjin::sdk::models;
 
-class TokenTest : public testing::Test {
+class TokenTest : public ModelsTestSuite {
 public:
     Token class_under_test;
+
+    constexpr static char POPULATED_JSON_OBJECT[] =
+            R"({"id":"1","name":"1","stateData":{},"configData":{},"variantMode":"NONE","variants":[{}, {}, {}],"createdAt":"1","updatedAt":"1"})";
 };
 
-TEST_F(TokenTest, SomeTest) {
+TEST_F(TokenTest, DeserializeEmptyStringFieldsDoNotHaveValues) {
     // Arrange
+    const std::string json;
 
     // Act
+    class_under_test.deserialize(json);
 
     // Assert
-    ASSERT_TRUE(false);
+    EXPECT_FALSE(class_under_test.get_id().has_value());
+    EXPECT_FALSE(class_under_test.get_name().has_value());
+    EXPECT_FALSE(class_under_test.get_state_data().has_value());
+    EXPECT_FALSE(class_under_test.get_config_data().has_value());
+    EXPECT_FALSE(class_under_test.get_variant_mode().has_value());
+    EXPECT_FALSE(class_under_test.get_variants().has_value());
+    EXPECT_FALSE(class_under_test.get_created_at().has_value());
+    EXPECT_FALSE(class_under_test.get_updated_at().has_value());
+}
+
+TEST_F(TokenTest, DeserializeEmptyJsonObjectFieldsDoNotHaveValues) {
+    // Arrange
+    const std::string json(EMPTY_JSON_OBJECT);
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    EXPECT_FALSE(class_under_test.get_id().has_value());
+    EXPECT_FALSE(class_under_test.get_name().has_value());
+    EXPECT_FALSE(class_under_test.get_state_data().has_value());
+    EXPECT_FALSE(class_under_test.get_config_data().has_value());
+    EXPECT_FALSE(class_under_test.get_variant_mode().has_value());
+    EXPECT_FALSE(class_under_test.get_variants().has_value());
+    EXPECT_FALSE(class_under_test.get_created_at().has_value());
+    EXPECT_FALSE(class_under_test.get_updated_at().has_value());
+}
+
+TEST_F(TokenTest, DeserializePopulatedJsonObjectFieldsHaveExpectedValues) {
+    // Arrange
+    const std::string expected_string("1");
+    const TokenStateData expected_state_data;
+    const TokenConfigData expected_config_data;
+    const TokenVariantMode expected_variant_mode = TokenVariantMode::NONE;
+    const std::string json(POPULATED_JSON_OBJECT);
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    EXPECT_EQ(expected_string, class_under_test.get_id().value());
+    EXPECT_EQ(expected_string, class_under_test.get_name().value());
+    EXPECT_EQ(expected_state_data, class_under_test.get_state_data().value());
+    EXPECT_EQ(expected_config_data, class_under_test.get_config_data().value());
+    EXPECT_EQ(expected_variant_mode, class_under_test.get_variant_mode().value());
+    EXPECT_TRUE(class_under_test.get_variants().has_value());
+    EXPECT_FALSE(class_under_test.get_variants().value().empty());
+    EXPECT_EQ(expected_string, class_under_test.get_created_at().value());
+    EXPECT_EQ(expected_string, class_under_test.get_updated_at().value());
+}
+
+TEST_F(TokenTest, DeserializePopulatedVariantsArrayHaveExpectedElements) {
+    // Arrange
+    const std::string json(R"({"variants":[{"id":1}, {"id":1}, {"id":1}]})");
+    TokenVariant expected;
+    expected.deserialize(R"({"id":1})");
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    ASSERT_TRUE(class_under_test.get_variants().has_value());
+    for (const TokenVariant& actual : class_under_test.get_variants().value()) {
+        EXPECT_EQ(expected, actual);
+    }
+}
+
+TEST_F(TokenTest, EqualityNeitherSideIsPopulatedReturnsTrue) {
+    // Arrange
+    Token lhs;
+    Token rhs;
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(TokenTest, EqualityBothSidesArePopulatedReturnsTrue) {
+    // Arrange
+    Token lhs;
+    Token rhs;
+    lhs.deserialize(POPULATED_JSON_OBJECT);
+    rhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(TokenTest, EqualityLeftSideIsPopulatedReturnsFalse) {
+    // Arrange
+    Token lhs;
+    Token rhs;
+    lhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
+}
+
+TEST_F(TokenTest, EqualityRightSideIsPopulatedReturnsFalse) {
+    // Arrange
+    Token lhs;
+    Token rhs;
+    rhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
 }
