6423df388b77b57e22095ed9210c79b374c49a63
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-15 18:31:37 -0800

    - Refactored Player model to implement IDeserializable instead ISerializable - Fully implemented Player model and its supporting models

--- include/enjinsdk/models/Player.hpp
+++ include/enjinsdk/models/Player.hpp
@@ -1,19 +1,60 @@
 #ifndef ENJINCPPSDK_PLAYER_HPP
 #define ENJINCPPSDK_PLAYER_HPP
 
-#include "enjinsdk/serialization/ISerializable.hpp"
+#include "enjinsdk/models/LinkingInfo.hpp"
+#include "enjinsdk/models/Wallet.hpp"
+#include "enjinsdk/serialization/IDeserializable.hpp"
+#include <optional>
+#include <string>
 
 namespace enjin::sdk::models {
 
-class Player : public enjin::sdk::serialization::ISerializable {
+/// \brief Models a player on the platform.
+class Player : public enjin::sdk::serialization::IDeserializable {
 public:
+    /// \brief Default constructor.
     Player() = default;
 
-    void deserialize(const char* json) override;
+    void deserialize(const std::string& json) override;
 
-    std::string serialize() override;
+    /// \brief Returns the ID of this player.
+    /// \return The player ID.
+    [[nodiscard]] const std::optional<std::string>& get_id() const;
 
-    // TODO: Implement fields and getters.
+    /// \brief Returns the linking information for this player.
+    /// \return The information.
+    [[nodiscard]] const std::optional<LinkingInfo>& get_linking_info() const;
+
+    /// \brief Returns the wallet for this player.
+    /// \return The wallet.
+    [[nodiscard]] const std::optional<Wallet>& get_wallet() const;
+
+    /// \brief Returns the datetime when this player was created.
+    /// \return The datetime.
+    /// \remarks The datetime is formatted using the ISO 8601 date format.
+    [[nodiscard]] const std::optional<std::string>& get_created_at() const;
+
+    /// \brief Returns the datetime when this player was last updated.
+    /// \return The datetime.
+    /// \remarks The datetime is formatted using the ISO 8601 date format.
+    [[nodiscard]] const std::optional<std::string>& get_updated_at() const;
+
+    bool operator==(const Player& rhs) const;
+
+    bool operator!=(const Player& rhs) const;
+
+private:
+    std::optional<std::string> id;
+    std::optional<LinkingInfo> linking_info;
+    std::optional<Wallet> wallet;
+    std::optional<std::string> created_at;
+    std::optional<std::string> updated_at;
+
+    constexpr static char ID_KEY[] = "id";
+    constexpr static char LINKING_INFO_KEY[] = "linkingInfo";
+    constexpr static char WALLET_KEY[] = "wallet";
+    constexpr static char CREATED_AT_KEY[] = "createdAt";
+    constexpr static char UPDATED_AT_KEY[] = "updatedAt";
 };
 
 }
---
+++ test/unit/models/LinkingInfoTest.cpp
@@ -0,0 +1,102 @@
+#include "ModelsTestSuite.hpp"
+#include "enjinsdk/models/LinkingInfo.hpp"
+#include <string>
+
+using namespace enjin::sdk::models;
+
+class LinkingInfoTest : public ModelsTestSuite {
+public:
+    LinkingInfo class_under_test;
+
+    constexpr static char POPULATED_JSON_OBJECT[] =
+            R"({"code":"1","qr":"1"})";
+};
+
+TEST_F(LinkingInfoTest, DeserializeEmptyStringFieldsDoNotHaveValues) {
+    // Arrange
+    const std::string json;
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    EXPECT_FALSE(class_under_test.get_code().has_value());
+    EXPECT_FALSE(class_under_test.get_qr().has_value());
+}
+
+TEST_F(LinkingInfoTest, DeserializeEmptyJsonObjectFieldsDoNotHaveValues) {
+    // Arrange
+    const std::string json(EMPTY_JSON_OBJECT);
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    EXPECT_FALSE(class_under_test.get_code().has_value());
+    EXPECT_FALSE(class_under_test.get_qr().has_value());
+}
+
+TEST_F(LinkingInfoTest, DeserializePopulatedJsonObjectFieldsHaveExpectedValues) {
+    // Arrange
+    const std::string expected_string("1");
+    const std::string json(POPULATED_JSON_OBJECT);
+
+    // Act
+    class_under_test.deserialize(json);
+
+    // Assert
+    EXPECT_EQ(expected_string, class_under_test.get_code().value());
+    EXPECT_EQ(expected_string, class_under_test.get_qr().value());
+}
+
+TEST_F(LinkingInfoTest, EqualityNeitherSideIsPopulatedReturnsTrue) {
+    // Arrange
+    LinkingInfo lhs;
+    LinkingInfo rhs;
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(LinkingInfoTest, EqualityBothSidesArePopulatedReturnsTrue) {
+    // Arrange
+    LinkingInfo lhs;
+    LinkingInfo rhs;
+    lhs.deserialize(POPULATED_JSON_OBJECT);
+    rhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(LinkingInfoTest, EqualityLeftSideIsPopulatedReturnsFalse) {
+    // Arrange
+    LinkingInfo lhs;
+    LinkingInfo rhs;
+    lhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
+}
+
+TEST_F(LinkingInfoTest, EqualityRightSideIsPopulatedReturnsFalse) {
+    // Arrange
+    LinkingInfo lhs;
+    LinkingInfo rhs;
+    rhs.deserialize(POPULATED_JSON_OBJECT);
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
+}
