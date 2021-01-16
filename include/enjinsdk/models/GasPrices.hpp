cc5731bbf9ca11070023b93486f1d3e58717baec
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-16 10:36:34 -0800

    Added GasPrices model

---
+++ include/enjinsdk/models/GasPrices.hpp
@@ -0,0 +1,55 @@
+#ifndef ENJINCPPSDK_GASPRICES_HPP
+#define ENJINCPPSDK_GASPRICES_HPP
+
+#include "enjinsdk/serialization/IDeserializable.hpp"
+#include <optional>
+
+namespace enjin::sdk::models {
+
+/// \brief Models gas prices on the platform.
+class GasPrices : enjin::sdk::serialization::IDeserializable {
+public:
+    /// \brief Default constructor.
+    GasPrices() = default;
+
+    void deserialize(const std::string& json) override;
+
+    /// \brief Returns the recommended safe gas price in Gwei.
+    /// \return The gas price.
+    /// \remarks Expected to be mined in less than 30 minutes.
+    [[nodiscard]] const std::optional<float>& get_safe_low() const;
+
+    /// \brief Returns the recommended average gas price in Gwei.
+    /// \return The gas price.
+    /// \remarks Expected to be mined in less than 5 minutes.
+    [[nodiscard]] const std::optional<float>& get_average() const;
+
+    /// \brief Returns the recommended fast gas price in Gwei.
+    /// \return The gas price.
+    /// \remarks Expected to be mined in less than 2 minutes.
+    [[nodiscard]] const std::optional<float>& get_fast() const;
+
+    /// \brief Returns the recommended fastest gas price in Gwei.
+    /// \return The gas price.
+    /// \remarks Expected to be mined in less than 30 seconds.
+    [[nodiscard]] const std::optional<float>& get_fastest() const;
+
+    bool operator==(const GasPrices& rhs) const;
+
+    bool operator!=(const GasPrices& rhs) const;
+
+private:
+    std::optional<float> safe_low;
+    std::optional<float> average;
+    std::optional<float> fast;
+    std::optional<float> fastest;
+
+    constexpr static char SAFE_LOW_KEY[] = "safeLow";
+    constexpr static char AVERAGE_KEY[] = "average";
+    constexpr static char FAST_KEY[] = "fast";
+    constexpr static char FASTEST_KEY[] = "fastest";
+};
+
+}
+
+#endif //ENJINCPPSDK_GASPRICES_HPP
