6a8fc946238b3375a82567e457fdc875db1a3837
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-18 17:43:39 -0800

    Added additional functions to RapidJsonUtils to more easily handle processing documents and serialization of objects

--- src/utils/RapidJsonUtils.cpp
+++ src/utils/RapidJsonUtils.cpp
@@ -2,6 +2,13 @@
 
 namespace enjin::sdk::utils {
 
+std::string document_to_string(const rapidjson::Document& document) {
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    document.Accept(writer);
+    return std::string(buffer.GetString());
+}
+
 std::vector<std::string> get_array_as_serialized_vector(const rapidjson::Document& document, const std::string& key) {
     if (key.empty() && document.IsArray()) {
         std::vector<std::string> v;
@@ -41,6 +48,54 @@ std::string get_object_as_string(const rapidjson::Document& document, const std:
     }
 
     throw std::exception("JSON element is not an object");
+}
+
+void set_member_assert(const rapidjson::Document& document, const std::string& key) {
+    if (!document.IsObject()) {
+        throw std::exception("Document is not a Json Object");
+    } else if (key.empty()) {
+        throw std::exception("Key string is empty");
+    }
+}
+
+void set_array_member_from_string_vector(rapidjson::Document& document,
+                                         const std::string& key,
+                                         const std::vector<std::string>& values) {
+    set_member_assert(document, key);
+
+    auto& allocator = document.GetAllocator();
+    rapidjson::Value v(rapidjson::kArrayType);
+    for (auto& e : values) {
+        rapidjson::Value e_value;
+        e_value.SetString(e.c_str(), allocator);
+        v.PushBack(e_value, allocator);
+    }
+
+    rapidjson::Value v_key;
+    v_key.SetString(key.c_str(), allocator);
+    document.AddMember(v_key, v, allocator);
+}
+
+void set_integer_member(rapidjson::Document& document, const std::string& key, int value) {
+    set_member_assert(document, key);
+
+    auto& allocator = document.GetAllocator();
+    rapidjson::Value v_key;
+    rapidjson::Value v;
+    v_key.SetString(key.c_str(), allocator);
+    v.SetInt(value);
+    document.AddMember(v_key, v, allocator);
+}
+
+void set_string_member(rapidjson::Document& document, const std::string& key, const std::string& value) {
+    set_member_assert(document, key);
+
+    auto& allocator = document.GetAllocator();
+    rapidjson::Value v_key;
+    rapidjson::Value v;
+    v_key.SetString(key.c_str(), allocator);
+    v.SetString(value.c_str(), allocator);
+    document.AddMember(v_key, v, allocator);
 }
 
 }
--- src/utils/RapidJsonUtils.hpp
+++ src/utils/RapidJsonUtils.hpp
@@ -2,6 +2,7 @@
 #define ENJINCPPSDK_RAPIDJSONUTILS_HPP
 
 #include "enjinsdk/serialization/IDeserializable.hpp"
+#include "enjinsdk/serialization/ISerializable.hpp"
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
@@ -10,6 +11,8 @@
 #include <vector>
 
 namespace enjin::sdk::utils {
+
+std::string document_to_string(const rapidjson::Document& document);
 
 std::vector<std::string> get_array_as_serialized_vector(const rapidjson::Document& document,
                                                         const std::string& key = "");
@@ -58,6 +61,47 @@ T get_object_as_type(const rapidjson::Document& document, const std::string& key
     t.deserialize(get_object_as_string(document, key));
     return t;
 }
+
+void set_member_assert(const rapidjson::Document& document, const std::string& key);
+
+template<class T>
+void set_array_member_from_type_vector(rapidjson::Document& document,
+                                       const std::string& key,
+                                       std::vector<T> values) {
+    static_assert(std::is_base_of<enjin::sdk::serialization::ISerializable, T>::value,
+                  "Class T does not inherit from ISerializable.");
+
+    set_member_assert(document, key);
+
+    auto& allocator = document.GetAllocator();
+    rapidjson::Value v(rapidjson::kArrayType);
+    for (auto& e : values) {
+        rapidjson::Value e_value(rapidjson::kObjectType);
+
+        /* Serializes the element into a JSON document to dynamically acquire its member name and values to convert
+         * into a JSON object that may then be stored in the JSON array.
+         */
+        rapidjson::Document e_document;
+        e_document.Parse(e.serialize().c_str());
+        for (auto& m : e_document.GetObject()) {
+            e_value.AddMember(m.name, m.value, allocator);
+        }
+
+        v.PushBack(e_value, allocator);
+    }
+
+    rapidjson::Value v_key;
+    v_key.SetString(key.c_str(), allocator);
+    document.AddMember(v_key, v, allocator);
+}
+
+void set_array_member_from_string_vector(rapidjson::Document& document,
+                                         const std::string& key,
+                                         const std::vector<std::string>& values);
+
+void set_integer_member(rapidjson::Document& document, const std::string& key, int value);
+
+void set_string_member(rapidjson::Document& document, const std::string& key, const std::string& value);
 
 }
 
