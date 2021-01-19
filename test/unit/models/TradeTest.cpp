44443ac6c76e3800ef46528a636b271bca6bef6b
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-19 11:01:49 -0800

    Added Trade model

---
+++ test/unit/models/TradeTest.cpp
@@ -0,0 +1,92 @@
+#include "ModelsTestSuite.hpp"
+#include "enjinsdk/models/Trade.hpp"
+#include <string>
+
+using namespace enjin::sdk::models;
+
+class TradeTest : public ModelsTestSuite {
+public:
+    Trade class_under_test;
+
+    constexpr static char POPULATED_JSON_OBJECT[] =
+            R"({"tokenId":"1","tokenIndex":"1","value":"1"})";
+
+    static Trade create_default_trade() {
+        return Trade().set_token_id("1")
+                      .set_token_index("1")
+                      .set_value("1");
+    }
+};
+
+TEST_F(TradeTest, SerializeNoSetFieldsReturnsEmptyJsonObject) {
+    // Arrange
+    const std::string expected(EMPTY_JSON_OBJECT);
+
+    // Act
+    std::string actual = class_under_test.serialize();
+
+    // Assert
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(TradeTest, SerializeSetFieldsReturnsExpectedJsonObject) {
+    // Arrange
+    const std::string expected(POPULATED_JSON_OBJECT);
+    class_under_test.set_token_id("1")
+                    .set_token_index("1")
+                    .set_value("1");
+
+    // Act
+    std::string actual = class_under_test.serialize();
+
+    // Assert
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(TradeTest, EqualityNeitherSideIsPopulatedReturnsTrue) {
+    // Arrange
+    Trade lhs;
+    Trade rhs;
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(TradeTest, EqualityBothSidesArePopulatedReturnsTrue) {
+    // Arrange
+    Trade lhs = create_default_trade();
+    Trade rhs = create_default_trade();
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_TRUE(actual);
+}
+
+TEST_F(TradeTest, EqualityLeftSideIsPopulatedReturnsFalse) {
+    // Arrange
+    Trade lhs = create_default_trade();
+    Trade rhs;
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
+}
+
+TEST_F(TradeTest, EqualityRightSideIsPopulatedReturnsFalse) {
+    // Arrange
+    Trade lhs;
+    Trade rhs = create_default_trade();
+
+    // Act
+    bool actual = lhs == rhs;
+
+    // Assert
+    ASSERT_FALSE(actual);
+}
