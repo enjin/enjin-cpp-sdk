545a5f64b06c0b2597bf1721849acf3d8a3f81b1
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-01-26 15:39:03 -0800

    Added request member functions to ISharedSchema

--- include/enjinsdk/shared/SharedSchema.hpp
+++ include/enjinsdk/shared/SharedSchema.hpp
@@ -9,6 +9,93 @@ namespace enjin::sdk::shared {
 
 class SharedSchema : public ISharedSchema,
                      public BaseSchema {
+public:
+    ~SharedSchema() override = default;
+
+    std::future<graphql::GraphqlResponse<models::Request>>
+    advanced_send_token_async(AdvancedSendToken& request) override;
+
+    graphql::GraphqlResponse<models::Request> advanced_send_token_sync(AdvancedSendToken& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> approve_enj_async(ApproveEnj& request) override;
+
+    graphql::GraphqlResponse<models::Request> approve_enj_sync(ApproveEnj& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> approve_enj_max_async(ApproveMaxEnj& request) override;
+
+    graphql::GraphqlResponse<models::Request> approve_enj_max_sync(ApproveMaxEnj& request) override;
+
+    std::future<graphql::GraphqlResponse<bool>> cancel_transaction_async(CancelTransaction& request) override;
+
+    graphql::GraphqlResponse<bool> cancel_transaction_sync(CancelTransaction& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> complete_trade_async(CompleteTrade& request) override;
+
+    graphql::GraphqlResponse<models::Request> complete_trade_sync(CompleteTrade& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> create_trade_async(CreateTrade& request) override;
+
+    graphql::GraphqlResponse<models::Request> create_trade_sync(CreateTrade& request) override;
+
+    std::future<graphql::GraphqlResponse<std::vector<models::Balance>>>
+    get_balances_async(GetBalances& request) override;
+
+    graphql::GraphqlResponse<std::vector<models::Balance>> get_balances_sync(GetBalances& request) override;
+
+    std::future<graphql::GraphqlResponse<models::GasPrices>> get_gas_prices_async(GetGasPrices& request) override;
+
+    graphql::GraphqlResponse<models::GasPrices> get_gas_prices_sync(GetGasPrices& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Platform>> get_platform_async(GetPlatform& request) override;
+
+    graphql::GraphqlResponse<models::Platform> get_platform_sync(GetPlatform& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Project>> get_project_async(GetProject& request) override;
+
+    graphql::GraphqlResponse<models::Project> get_project_sync(GetProject& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> get_request_async(GetRequest& request) override;
+
+    graphql::GraphqlResponse<models::Request> get_request_sync(GetRequest& request) override;
+
+    std::future<graphql::GraphqlResponse<std::vector<models::Request>>>
+    get_requests_async(GetRequests& request) override;
+
+    graphql::GraphqlResponse<std::vector<models::Request>> get_requests_sync(GetRequests& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Token>> get_token_async(GetToken& request) override;
+
+    graphql::GraphqlResponse<models::Token> get_token_sync(GetToken& request) override;
+
+    std::future<graphql::GraphqlResponse<std::vector<models::Token>>> get_tokens_async(GetTokens& request) override;
+
+    graphql::GraphqlResponse<std::vector<models::Token>> get_tokens_sync(GetTokens& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> melt_token_async(MeltToken& request) override;
+
+    graphql::GraphqlResponse<models::Request> melt_token_sync(MeltToken& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> message_async(Message& request) override;
+
+    graphql::GraphqlResponse<models::Request> message_sync(Message& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> reset_enj_approval_async(ResetEnjApproval& request) override;
+
+    graphql::GraphqlResponse<models::Request> reset_enj_approval_sync(ResetEnjApproval& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> send_enj_async(SendEnj& request) override;
+
+    graphql::GraphqlResponse<models::Request> send_enj_sync(SendEnj& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>> send_token_async(SendToken& request) override;
+
+    graphql::GraphqlResponse<models::Request> send_token_sync(SendToken& request) override;
+
+    std::future<graphql::GraphqlResponse<models::Request>>
+    set_approval_for_all_async(SetApprovalForAll& request) override;
+
+    graphql::GraphqlResponse<models::Request> set_approval_for_all_sync(SetApprovalForAll& request) override;
+
 protected:
     SharedSchema(const enjin::sdk::TrustedPlatformMiddleware& middleware, const std::string& schema);
 };
--- src/schemas/shared/SharedSchema.cpp
+++ src/schemas/shared/SharedSchema.cpp
@@ -6,4 +6,169 @@ SharedSchema::SharedSchema(const enjin::sdk::TrustedPlatformMiddleware& middlewa
         : BaseSchema(middleware, schema) {
 }
 
+std::future<graphql::GraphqlResponse<models::Request>>
+SharedSchema::advanced_send_token_async(AdvancedSendToken& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::advanced_send_token_sync(AdvancedSendToken& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::approve_enj_async(ApproveEnj& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::approve_enj_sync(ApproveEnj& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::approve_enj_max_async(ApproveMaxEnj& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::approve_enj_max_sync(ApproveMaxEnj& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<bool>> SharedSchema::cancel_transaction_async(CancelTransaction& request) {
+    return send_request_for_one<bool>(request);
+}
+
+graphql::GraphqlResponse<bool> SharedSchema::cancel_transaction_sync(CancelTransaction& request) {
+    return send_request_for_one<bool>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::complete_trade_async(CompleteTrade& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::complete_trade_sync(CompleteTrade& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::create_trade_async(CreateTrade& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::create_trade_sync(CreateTrade& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<std::vector<models::Balance>>>
+SharedSchema::get_balances_async(GetBalances& request) {
+    return send_request_for_many<models::Balance>(request);
+}
+
+graphql::GraphqlResponse<std::vector<models::Balance>> SharedSchema::get_balances_sync(GetBalances& request) {
+    return send_request_for_many<models::Balance>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::GasPrices>> SharedSchema::get_gas_prices_async(GetGasPrices& request) {
+    return send_request_for_one<models::GasPrices>(request);
+}
+
+graphql::GraphqlResponse<models::GasPrices> SharedSchema::get_gas_prices_sync(GetGasPrices& request) {
+    return send_request_for_one<models::GasPrices>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Platform>> SharedSchema::get_platform_async(GetPlatform& request) {
+    return send_request_for_one<models::Platform>(request);
+}
+
+graphql::GraphqlResponse<models::Platform> SharedSchema::get_platform_sync(GetPlatform& request) {
+    return send_request_for_one<models::Platform>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Project>> SharedSchema::get_project_async(GetProject& request) {
+    return send_request_for_one<models::Project>(request);
+}
+
+graphql::GraphqlResponse<models::Project> SharedSchema::get_project_sync(GetProject& request) {
+    return send_request_for_one<models::Project>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::get_request_async(GetRequest& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::get_request_sync(GetRequest& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<std::vector<models::Request>>>
+SharedSchema::get_requests_async(GetRequests& request) {
+    return send_request_for_many<models::Request>(request);
+}
+
+graphql::GraphqlResponse<std::vector<models::Request>> SharedSchema::get_requests_sync(GetRequests& request) {
+    return send_request_for_many<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Token>> SharedSchema::get_token_async(GetToken& request) {
+    return send_request_for_one<models::Token>(request);
+}
+
+graphql::GraphqlResponse<models::Token> SharedSchema::get_token_sync(GetToken& request) {
+    return send_request_for_one<models::Token>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<std::vector<models::Token>>> SharedSchema::get_tokens_async(GetTokens& request) {
+    return send_request_for_many<models::Token>(request);
+}
+
+graphql::GraphqlResponse<std::vector<models::Token>> SharedSchema::get_tokens_sync(GetTokens& request) {
+    return send_request_for_many<models::Token>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::melt_token_async(MeltToken& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::melt_token_sync(MeltToken& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::message_async(Message& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::message_sync(Message& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>>
+SharedSchema::reset_enj_approval_async(ResetEnjApproval& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::reset_enj_approval_sync(ResetEnjApproval& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::send_enj_async(SendEnj& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::send_enj_sync(SendEnj& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>> SharedSchema::send_token_async(SendToken& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::send_token_sync(SendToken& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
+std::future<graphql::GraphqlResponse<models::Request>>
+SharedSchema::set_approval_for_all_async(SetApprovalForAll& request) {
+    return send_request_for_one<models::Request>(request);
+}
+
+graphql::GraphqlResponse<models::Request> SharedSchema::set_approval_for_all_sync(SetApprovalForAll& request) {
+    return send_request_for_one<models::Request>(request).get();
+}
+
 }
