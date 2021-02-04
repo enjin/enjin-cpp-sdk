5366af120e1e0f2d805106366e77bd4b522a9c5d
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-03 16:21:09 -0800

    Added EventListenerRegistration class and supporting configuration class

---
+++ include/enjinsdk/EventListenerRegistration.hpp
@@ -0,0 +1,84 @@
+#ifndef ENJINCPPSDK_EVENTLISTENERREGISTRATION_HPP
+#define ENJINCPPSDK_EVENTLISTENERREGISTRATION_HPP
+
+#include "enjinsdk/IEventListener.hpp"
+#include "enjinsdk/models/EventType.hpp"
+#include <functional>
+#include <memory>
+
+namespace enjin::sdk::events {
+
+/// \brief Registration wrapper for a IEventListener.
+class EventListenerRegistration {
+public:
+    /// \brief Configuration class for building an EventListenerRegistration. To be used internally.
+    class RegistrationListenerConfiguration {
+    public:
+        RegistrationListenerConfiguration() = delete;
+
+        /// \brief Constructs a configuration builder for a registration.
+        /// \param listener The shared listener that the registration will receive.
+        explicit RegistrationListenerConfiguration(std::shared_ptr<IEventListener> listener);
+
+        /// \brief Default destructor.
+        ~RegistrationListenerConfiguration() = default;
+
+        /// \brief Sets the matcher to be used for the registration.
+        /// \param matcher The matcher.
+        /// \return This for chaining.
+        RegistrationListenerConfiguration& with_matcher(std::function<bool(models::EventType)> matcher);
+
+        /// \brief Sets the matcher to be used for the registration that allows the passed event types.
+        /// \param types The event types to match.
+        /// \return This for chaining.
+        RegistrationListenerConfiguration& with_allowed_events(const std::vector<models::EventType>& types);
+
+        /// \brief Sets the matcher to be used for the registration that ignore the passed event types.
+        /// \param types The event types to not match.
+        /// \return This for chaining.
+        RegistrationListenerConfiguration& with_ignored_events(const std::vector<models::EventType>& types);
+
+        /// \brief Creates the registration and provides the unique pointer for it.
+        /// \return The unique pointer for the registration.
+        std::unique_ptr<EventListenerRegistration> create();
+
+        /// \brief Returns a reference to the listener.
+        /// \return The reference to the listener.
+        [[nodiscard]] const IEventListener& get_listener() const;
+
+    private:
+        std::shared_ptr<IEventListener> listener;
+        std::function<bool(models::EventType)> matcher = ALLOW_ALL_MATCHER;
+    };
+
+    EventListenerRegistration() = delete;
+
+    /// \brief Default destructor.
+    ~EventListenerRegistration() = default;
+
+    /// \brief Returns a reference to the listener.
+    /// \return The listener.
+    [[nodiscard]] IEventListener& get_listener() const;
+
+    /// \brief Returns the event type matcher associated with listener.
+    /// \return The matcher.
+    [[nodiscard]] const std::function<bool(models::EventType)>& get_matcher() const;
+
+    /// \brief A matcher that returns true for any event type.
+    inline static std::function<bool(models::EventType)> const ALLOW_ALL_MATCHER = [](models::EventType type) {
+        return true;
+    };
+
+protected:
+    explicit EventListenerRegistration(std::shared_ptr<IEventListener> listener);
+
+    EventListenerRegistration(std::shared_ptr<IEventListener> listener, std::function<bool(models::EventType)>  matcher);
+
+private:
+    std::shared_ptr<IEventListener> listener;
+    std::function<bool(models::EventType)> matcher;
+};
+
+}
+
+#endif //ENJINCPPSDK_EVENTLISTENERREGISTRATION_HPP
---
+++ test/unit/events/RegistrationListenerConfigurationTest.cpp
@@ -0,0 +1,144 @@
+#include "../../utils//MockEventListener.hpp"
+#include "enjinsdk/EventListenerRegistration.hpp"
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+using namespace enjin::sdk::events;
+
+class RegistrationListenerConfigurationTest : public testing::Test {
+public:
+    inline static const std::vector<enjin::sdk::models::EventType> types = {
+            enjin::sdk::models::EventType::UNKNOWN,
+            enjin::sdk::models::EventType::APP_CREATED,
+            enjin::sdk::models::EventType::APP_DELETED,
+            enjin::sdk::models::EventType::APP_LINKED,
+            enjin::sdk::models::EventType::APP_LOCKED,
+            enjin::sdk::models::EventType::APP_UNLINKED,
+            enjin::sdk::models::EventType::APP_UNLOCKED,
+            enjin::sdk::models::EventType::APP_UPDATED,
+            enjin::sdk::models::EventType::BLOCKCHAIN_LOG_PROCESSED,
+            enjin::sdk::models::EventType::MESSAGE_PROCESSED,
+            enjin::sdk::models::EventType::PLAYER_CREATED,
+            enjin::sdk::models::EventType::PLAYER_DELETED,
+            enjin::sdk::models::EventType::PLAYER_LINKED,
+            enjin::sdk::models::EventType::PLAYER_UNLINKED,
+            enjin::sdk::models::EventType::PLAYER_UPDATED,
+            enjin::sdk::models::EventType::TOKEN_CREATED,
+            enjin::sdk::models::EventType::TOKEN_MELTED,
+            enjin::sdk::models::EventType::TOKEN_MINTED,
+            enjin::sdk::models::EventType::TOKEN_TRANSFERRED,
+            enjin::sdk::models::EventType::TOKEN_UPDATED,
+            enjin::sdk::models::EventType::TRADE_COMPLETED,
+            enjin::sdk::models::EventType::TRADE_CREATED,
+            enjin::sdk::models::EventType::TRANSACTION_BROADCAST,
+            enjin::sdk::models::EventType::TRANSACTION_CANCELED,
+            enjin::sdk::models::EventType::TRANSACTION_DROPPED,
+            enjin::sdk::models::EventType::TRANSACTION_EXECUTED,
+            enjin::sdk::models::EventType::TRANSACTION_FAILED,
+            enjin::sdk::models::EventType::TRANSACTION_PENDING,
+            enjin::sdk::models::EventType::TRANSACTION_PROCESSING,
+            enjin::sdk::models::EventType::TRANSACTION_UPDATED,
+    };
+};
+
+TEST_F(RegistrationListenerConfigurationTest, RegistrationHasConfiguredListenerWhenCreated) {
+    // Arrange
+    std::shared_ptr<IEventListener> mock_listener(new MockEventListener);
+    EventListenerRegistration::RegistrationListenerConfiguration configuration(mock_listener);
+
+    // Act
+    std::unique_ptr<EventListenerRegistration> registration = configuration.create();
+
+    // Assert
+    ASSERT_EQ(mock_listener.get(), &registration->get_listener());
+}
+
+TEST_F(RegistrationListenerConfigurationTest, NoGivenMatcherRegistrationIsCreatedWithAllowAllMatcher) {
+    // Arrange
+    std::shared_ptr<IEventListener> mock_listener(new MockEventListener);
+    EventListenerRegistration::RegistrationListenerConfiguration configuration(mock_listener);
+
+    // Act
+    std::unique_ptr<EventListenerRegistration> registration = configuration.create();
+
+    // Assert
+    for (auto t : types) {
+        EXPECT_TRUE(registration->get_matcher()(t));
+    }
+}
+
+TEST_F(RegistrationListenerConfigurationTest, WithMatcherRegistrationIsCreatedWithCorrectMatcher) {
+    // Arrange
+    const std::function<bool(enjin::sdk::models::EventType)> matcher = [](enjin::sdk::models::EventType type) {
+        return false;
+    };
+    std::shared_ptr<IEventListener> mock_listener(new MockEventListener);
+    EventListenerRegistration::RegistrationListenerConfiguration configuration(mock_listener);
+
+    // Act
+    std::unique_ptr<EventListenerRegistration> registration = configuration.with_matcher(matcher)
+                                                                           .create();
+
+    // Assert
+    for (auto t : types) {
+        EXPECT_FALSE(registration->get_matcher()(t));
+    }
+}
+
+TEST_F(RegistrationListenerConfigurationTest, WithAllowedEventsRegistrationIsCreatedWithCorrectMatcher) {
+    // Arrange
+    const std::vector<enjin::sdk::models::EventType> allowed_events = {
+            enjin::sdk::models::EventType::PLAYER_LINKED,
+            enjin::sdk::models::EventType::PLAYER_UNLINKED,
+            enjin::sdk::models::EventType::PLAYER_UPDATED,
+            enjin::sdk::models::EventType::TOKEN_MELTED,
+            enjin::sdk::models::EventType::TOKEN_MINTED,
+            enjin::sdk::models::EventType::TOKEN_TRANSFERRED,
+            enjin::sdk::models::EventType::TOKEN_UPDATED,
+    };
+    std::shared_ptr<IEventListener> mock_listener(new MockEventListener);
+    EventListenerRegistration::RegistrationListenerConfiguration configuration(mock_listener);
+
+    // Act
+    std::unique_ptr<EventListenerRegistration> registration = configuration.with_allowed_events(allowed_events)
+                                                                           .create();
+
+    // Assert
+    for (auto t : types) {
+        if (std::find(allowed_events.begin(), allowed_events.end(), t) != allowed_events.end()) {
+            EXPECT_TRUE(registration->get_matcher()(t));
+        } else {
+            EXPECT_FALSE(registration->get_matcher()(t));
+        }
+    }
+}
+
+TEST_F(RegistrationListenerConfigurationTest, WithIgnoredEventsRegistrationIsCreatedWithCorrectMatcher) {
+    // Arrange
+    const std::vector<enjin::sdk::models::EventType> ignored_events = {
+            enjin::sdk::models::EventType::PLAYER_LINKED,
+            enjin::sdk::models::EventType::PLAYER_UNLINKED,
+            enjin::sdk::models::EventType::PLAYER_UPDATED,
+            enjin::sdk::models::EventType::TOKEN_MELTED,
+            enjin::sdk::models::EventType::TOKEN_MINTED,
+            enjin::sdk::models::EventType::TOKEN_TRANSFERRED,
+            enjin::sdk::models::EventType::TOKEN_UPDATED,
+    };
+    std::shared_ptr<IEventListener> mock_listener(new MockEventListener);
+    EventListenerRegistration::RegistrationListenerConfiguration configuration(mock_listener);
+
+    // Act
+    std::unique_ptr<EventListenerRegistration> registration = configuration.with_ignored_events(ignored_events)
+                                                                           .create();
+
+    // Assert
+    for (auto t : types) {
+        if (std::find(ignored_events.begin(), ignored_events.end(), t) == ignored_events.end()) {
+            EXPECT_TRUE(registration->get_matcher()(t));
+        } else {
+            EXPECT_FALSE(registration->get_matcher()(t));
+        }
+    }
+}
