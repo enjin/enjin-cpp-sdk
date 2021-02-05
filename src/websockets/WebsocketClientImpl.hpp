95cbe9e3d16b2432c19af64102a003f18afdd9a6
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-05 13:24:15 -0800

    - Added IWebsocketClient interface - Added initial implementation class for a websocket client

---
+++ src/websockets/WebsocketClientImpl.hpp
@@ -0,0 +1,37 @@
+#ifndef ENJINCPPSDK_WEBSOCKETCLIENTIMPL_HPP
+#define ENJINCPPSDK_WEBSOCKETCLIENTIMPL_HPP
+
+#include "cpprest/ws_client.h"
+#include "enjinsdk/IWebsocketClient.hpp"
+#include <functional>
+#include <future>
+#include <string>
+
+namespace enjin::sdk::websockets {
+
+/// \brief Implementation class of a websocket client.
+class WebsocketClientImpl : public IWebsocketClient {
+public:
+    WebsocketClientImpl() = default;
+
+    ~WebsocketClientImpl() override = default;
+
+    std::future<void> connect(const std::string& uri) override;
+
+    std::future<void> send(const std::string& data) override;
+
+    void set_close_handler(const std::function<void(int close_status,
+                                                    const std::string& message,
+                                                    const std::error_code& error)>& handler) override;
+
+    void set_message_handler(const std::function<void(const std::string& message)>& handler) override;
+
+    std::future<void> close() override;
+
+private:
+    web::web_sockets::client::websocket_callback_client ws_client;
+};
+
+}
+
+#endif //ENJINCPPSDK_WEBSOCKETCLIENTIMPL_HPP
