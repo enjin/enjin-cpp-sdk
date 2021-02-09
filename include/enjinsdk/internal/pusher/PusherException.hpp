e7b15da5ce273aee941ef6c06ca15a7c76cb52d3
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-08 16:17:58 -0800

    Added PusherException

---
+++ include/enjinsdk/internal/pusher/PusherException.hpp
@@ -0,0 +1,29 @@
+#ifndef ENJINCPPSDK_PUSHEREXCEPTION_HPP
+#define ENJINCPPSDK_PUSHEREXCEPTION_HPP
+
+#include <exception>
+#include <string>
+
+namespace enjin::pusher {
+
+/// \brief Exception class for Pusher events.
+class PusherException : public std::exception {
+public:
+    /// \brief Constructs the exception with a message and error code.
+    /// \param message The message.
+    /// \param error_code The error code.
+    PusherException(const std::string& message, int error_code);
+
+    ~PusherException() noexcept override = default;
+
+    /// \brief Returns the error exception code.
+    /// \return The error code.
+    [[nodiscard]] int get_error_code() const;
+
+private:
+    int error_code;
+};
+
+}
+
+#endif //ENJINCPPSDK_PUSHEREXCEPTION_HPP
