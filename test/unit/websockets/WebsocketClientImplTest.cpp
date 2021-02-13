89c9d62b6ca6a71732f99f12726521fb7808a5b7
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-12 16:43:33 -0800

    - Added VerificationSuite for basic function call verification - Improved unit tests for WebsocketClientImplTest

--- test/unit/websockets/WebsocketClientImplTest.cpp
+++ test/unit/websockets/WebsocketClientImplTest.cpp
@@ -1,45 +1,18 @@
 #include "gtest/gtest.h"
 #include "WebsocketClientImpl.hpp"
 #include "../../utils/MockWebsocketServer.hpp"
-#include <atomic>
-#include <chrono>
-#include <mutex>
+#include "../../utils/VerificationSuite.hpp"
 #include <string>
 
 using namespace enjin::sdk::websockets;
 using namespace enjin::test::utils;
 
-class WebsocketClientImplTest : public testing::Test {
+class WebsocketClientImplTest : public VerificationSuite {
 public:
     static constexpr char URI[] = "ws://localhost:9980/ws";
 
     WebsocketClientImpl class_under_test;
     MockWebsocketServer mock_server;
-
-    /* Members for setting expectations and verifying calls to functional arguments.
-     */
-    std::atomic_int call_counter = 0;
-    int expected_count = 0;
-
-    void set_expected_call_count(int count) {
-        expected_count = count;
-    }
-
-    void increment_call_counter() {
-        call_counter++;
-    }
-
-    void verify_call_count(int time = 0) const {
-        // Waits and gives the calls an opportunity to be made if asynchronous
-        std::mutex mutex;
-        std::unique_lock<std::mutex> lock(mutex);
-        std::condition_variable condition_variable;
-        condition_variable.wait_for(lock, std::chrono::seconds(time), [this]() {
-            return expected_count == call_counter;
-        });
-
-        EXPECT_EQ(expected_count, call_counter);
-    }
 
 protected:
     void SetUp() override {
@@ -70,24 +43,22 @@ TEST_F(WebsocketClientImplConnectCloseTest, CloseClientCosesConnectionWithServer
 
 TEST_F(WebsocketClientImplTest, ServerClosesConnectionClientReceivesExpected) {
     // Arrange - Data
-    const int expected_status = (int) web::websockets::client::websocket_close_status::normal;
-    const std::string expected_message("Normal");
-    const int expected_error_value = 0;
     TestWebsocketMessage message;
     message.set_type(WebsocketMessageType::WEBSOCKET_CLOSE_TYPE);
 
     // Arrange - Expectations
-    class_under_test.set_close_handler([expected_status, expected_message, expected_error_value]
-                                               (int actual_status,
-                                                const std::string& actual_message,
-                                                const std::error_code& actual_error) {
-        EXPECT_EQ(expected_status, actual_status);
-        EXPECT_EQ(expected_message, actual_message);
-        EXPECT_EQ(expected_error_value, actual_error.value());
+    class_under_test.set_close_handler([this](int actual_status,
+                                              const std::string& actual_message,
+                                              const std::error_code& actual_error) {
+        increment_call_counter();
     });
+    set_expected_call_count(1);
 
     // Act
     mock_server.send_message(message);
+
+    // Verify
+    verify_call_count(2);
 
     // Assert (see: Arrange - Expectations)
 }
@@ -155,15 +126,21 @@ TEST_F(WebsocketClientImplTest, SendServerReceivesMessage) {
     const std::string expected("message");
 
     // Arrange - Expectations
-    mock_server.next_message([expected](const TestWebsocketMessage& message) {
+    mock_server.next_message([this, expected](const TestWebsocketMessage& message) {
+        increment_call_counter();
+
         auto data = message.get_data();
         std::string actual(data.begin(), data.end());
 
         EXPECT_EQ(expected, actual);
     });
+    set_expected_call_count(1);
 
     // Act
     class_under_test.send(expected).wait();
+
+    // Verify
+    verify_call_count(2);
 
     // Assert (see: Arrange - Expectations)
 }
