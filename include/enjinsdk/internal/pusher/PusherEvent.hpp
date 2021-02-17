89ab21f26fdfc37bcca7a7ae92439958f5e5018d
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-16 18:06:53 -0800

    - Added documentation to PusherEvent - Added documentation to PusherOptions

--- include/enjinsdk/internal/pusher/PusherEvent.hpp
+++ include/enjinsdk/internal/pusher/PusherEvent.hpp
@@ -7,21 +7,30 @@
 
 namespace enjin::pusher {
 
-/// \brief TODO
+/// \brief Class for modeling Pusher events.
 class PusherEvent : public sdk::serialization::IDeserializable {
 public:
+    /// \brief Default constructor.
     PusherEvent() = default;
 
     ~PusherEvent() override = default;
 
     void deserialize(const std::string& json) override;
 
+    /// \brief Returns the user ID of the event.
+    /// \return The optional for the user ID.
     [[nodiscard]] const std::optional<std::string>& get_user_id() const;
 
+    /// \brief Returns the channel name of the event.
+    /// \return The optional for the channel name.
     [[nodiscard]] const std::optional<std::string>& get_channel_name() const;
 
+    /// \brief Returns the name of the event.
+    /// \return The optional for the event name.
     [[nodiscard]] const std::optional<std::string>& get_event_name() const;
 
+    /// \brief Returns the data of the event.
+    /// \return The optional for the data.
     [[nodiscard]] const std::optional<std::string>& get_data() const;
 
     bool operator==(const PusherEvent& rhs) const;
--- include/enjinsdk/internal/pusher/PusherOptions.hpp
+++ include/enjinsdk/internal/pusher/PusherOptions.hpp
@@ -6,21 +6,35 @@
 
 namespace enjin::pusher {
 
-/// \brief TODO
+/// \brief Class for modeling options for a Pusher client.
 class PusherOptions {
 public:
+    /// \brief Default constructor.
     PusherOptions() = default;
 
+    /// \brief Default destructor.
     ~PusherOptions() = default;
 
+    /// \brief Returns the websocket host to be used by the client.
+    /// \return The host.
     [[nodiscard]] std::string host() const;
 
+    /// \brief Determines if encryption is to be used.
+    /// \return Whether encryption is to be used.
     [[nodiscard]] bool is_encrypted() const;
 
+    /// \brief Sets the cluster.
+    /// \param cluster The cluster.
+    /// \return This for chaining.
     PusherOptions& set_cluster(const std::string& cluster);
 
+    /// \brief Sets whether encryption is to be used.
+    /// \param encrypted The state.
+    /// \return This for chaining.
     PusherOptions& set_encrypted(bool encrypted);
 
+    /// \brief Returns the cluster.
+    /// \return The optional for the cluster.
     [[nodiscard]] const std::optional<std::string>& get_cluster() const;
 
 private:
