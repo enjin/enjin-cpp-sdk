05cc1521ce3ea1f9e09fedd1e0a0a6c2e94aa15a
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-02-23 16:19:07 -0800

    - Added no-arg constructor for EventTypeDef - Made primary constructor for EventTypeDef public - Static map in EventTypeDef now has values as smart pointers

--- src/events/EventTypeDef.hpp
+++ src/events/EventTypeDef.hpp
@@ -3,6 +3,7 @@
 
 #include "enjinsdk/models/EventType.hpp"
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -10,7 +11,9 @@ namespace enjin::sdk::events {
 
 class EventTypeDef {
 public:
-    EventTypeDef() = delete;
+    EventTypeDef();
+
+    EventTypeDef(models::EventType type, std::string key, std::vector<std::string> channels);
 
     ~EventTypeDef() = default;
 
@@ -42,102 +45,13 @@ private:
     std::vector<std::string> channels;
     std::string name;
 
-    EventTypeDef(models::EventType type, std::string key, std::vector<std::string> channels);
+    static std::unique_ptr<EventTypeDef> create(models::EventType type,
+                                                const std::string& key,
+                                                std::vector<std::string> channels);
 
-    static EventTypeDef* create(models::EventType type, const std::string& key, std::vector<std::string> channels);
+    static std::map<models::EventType, std::unique_ptr<EventTypeDef>> create_map();
 
-    inline static std::map<models::EventType, EventTypeDef*> map = {
-            {models::EventType::UNKNOWN,                  create(models::EventType::UNKNOWN,
-                                                                 "",
-                                                                 {""})},
-            {models::EventType::PROJECT_CREATED,          create(models::EventType::PROJECT_CREATED,
-                                                                 "EnjinCloud\\Events\\ProjectCreated",
-                                                                 {"project"})},
-            {models::EventType::PROJECT_DELETED,          create(models::EventType::PROJECT_DELETED,
-                                                                 "EnjinCloud\\Events\\ProjectDeleted",
-                                                                 {"project"})},
-            {models::EventType::PROJECT_LINKED,           create(models::EventType::PROJECT_LINKED,
-                                                                 "EnjinCloud\\Events\\ProjectLinked",
-                                                                 {"project", "wallet"})},
-            {models::EventType::PROJECT_LOCKED,           create(models::EventType::PROJECT_LOCKED,
-                                                                 "EnjinCloud\\Events\\ProjectLocked",
-                                                                 {"project"})},
-            {models::EventType::PROJECT_UNLINKED,         create(models::EventType::PROJECT_UNLINKED,
-                                                                 "EnjinCloud\\Events\\ProjectUnlinked",
-                                                                 {"project", "wallet"})},
-            {models::EventType::PROJECT_UNLOCKED,         create(models::EventType::PROJECT_UNLOCKED,
-                                                                 "EnjinCloud\\Events\\ProjectUnlocked",
-                                                                 {"project"})},
-            {models::EventType::PROJECT_UPDATED,          create(models::EventType::PROJECT_UPDATED,
-                                                                 "EnjinCloud\\Events\\ProjectUpdated",
-                                                                 {"project"})},
-            {models::EventType::BLOCKCHAIN_LOG_PROCESSED, create(models::EventType::BLOCKCHAIN_LOG_PROCESSED,
-                                                                 "EnjinCloud\\Events\\BlockchainLogProcessed",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::MESSAGE_PROCESSED,        create(models::EventType::MESSAGE_PROCESSED,
-                                                                 "EnjinCloud\\Events\\MessageProcessed",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::PLAYER_CREATED,           create(models::EventType::PLAYER_CREATED,
-                                                                 "EnjinCloud\\Events\\PlayerCreated",
-                                                                 {"project", "player"})},
-            {models::EventType::PLAYER_DELETED,           create(models::EventType::PLAYER_DELETED,
-                                                                 "EnjinCloud\\Events\\PlayerDeleted",
-                                                                 {"project", "player"})},
-            {models::EventType::PLAYER_LINKED,            create(models::EventType::PLAYER_LINKED,
-                                                                 "EnjinCloud\\Events\\PlayerLinked",
-                                                                 {"project", "player", "wallet"})},
-            {models::EventType::PLAYER_UNLINKED,          create(models::EventType::PLAYER_UNLINKED,
-                                                                 "EnjinCloud\\Events\\PlayerUnlinked",
-                                                                 {"project", "player", "wallet"})},
-            {models::EventType::PLAYER_UPDATED,           create(models::EventType::PLAYER_UPDATED,
-                                                                 "EnjinCloud\\Events\\PlayerUpdated",
-                                                                 {"project", "player"})},
-            {models::EventType::ASSET_CREATED,            create(models::EventType::ASSET_CREATED,
-                                                                 "EnjinCloud\\Events\\AssetCreated",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::ASSET_MELTED,             create(models::EventType::ASSET_MELTED,
-                                                                 "EnjinCloud\\Events\\AssetMelted",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::ASSET_MINTED,             create(models::EventType::ASSET_MINTED,
-                                                                 "EnjinCloud\\Events\\AssetMinted",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::ASSET_TRANSFERRED,        create(models::EventType::ASSET_TRANSFERRED,
-                                                                 "EnjinCloud\\Events\\AssetTransferred",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::ASSET_UPDATED,            create(models::EventType::ASSET_UPDATED,
-                                                                 "EnjinCloud\\Events\\AssetUpdated",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRADE_COMPLETED,          create(models::EventType::TRADE_COMPLETED,
-                                                                 "EnjinCloud\\Events\\TradeCompleted",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRADE_CREATED,            create(models::EventType::TRADE_CREATED,
-                                                                 "EnjinCloud\\Events\\TradeCreated",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_BROADCAST,    create(models::EventType::TRANSACTION_BROADCAST,
-                                                                 "EnjinCloud\\Events\\TransactionBroadcast",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_CANCELED,     create(models::EventType::TRANSACTION_CANCELED,
-                                                                 "EnjinCloud\\Events\\TransactionCanceled",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_DROPPED,      create(models::EventType::TRANSACTION_DROPPED,
-                                                                 "EnjinCloud\\Events\\TransactionDropped",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_EXECUTED,     create(models::EventType::TRANSACTION_EXECUTED,
-                                                                 "EnjinCloud\\Events\\TransactionExecuted",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_FAILED,       create(models::EventType::TRANSACTION_FAILED,
-                                                                 "EnjinCloud\\Events\\TransactionFailed",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_PENDING,      create(models::EventType::TRANSACTION_PENDING,
-                                                                 "EnjinCloud\\Events\\TransactionPending",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_PROCESSING,   create(models::EventType::TRANSACTION_PROCESSING,
-                                                                 "EnjinCloud\\Events\\TransactionProcessing",
-                                                                 {"project", "asset", "wallet"})},
-            {models::EventType::TRANSACTION_UPDATED,      create(models::EventType::TRANSACTION_UPDATED,
-                                                                 "EnjinCloud\\Events\\TransactionUpdated",
-                                                                 {"project", "asset", "wallet"})},
-    };
+    inline static std::map<models::EventType, std::unique_ptr<EventTypeDef>> map = create_map();
 };
 
 }
