9de6aa3205a350f07f866e6f1b6dc398dda5506e
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-03-12 23:39:14 -0800

    Separated unit tests for PusherEventService to have tests which rely on a mock websocket client/server to be built only when the SDK targets its websocket dependency

---
+++ test/include/MockWebsocketClient.hpp
@@ -0,0 +1,40 @@
+#ifndef ENJINSDK_MOCKWEBSOCKETCLIENT_HPP
+#define ENJINSDK_MOCKWEBSOCKETCLIENT_HPP
+
+#include "enjinsdk/IWebsocketClient.hpp"
+#include "gmock/gmock.h"
+#include <functional>
+#include <string>
+
+namespace enjin::test::utils {
+
+class MockWebsocketClient : public enjin::sdk::websockets::IWebsocketClient {
+public:
+    MOCK_METHOD(void, connect, (const std::string& uri), (override));
+
+    MOCK_METHOD(void, close, (), (override));
+
+    MOCK_METHOD(void, close, (int status_code, const std::string& reason), (override));
+
+    MOCK_METHOD(void, send, (const std::string& data), (override));
+
+    MOCK_METHOD(void, set_open_handler, (const std::function<void()>& handler), (override));
+
+    MOCK_METHOD(void,
+                set_close_handler,
+                (const std::function<void(int close_status, const std::string& message)>& handler),
+                (override));
+
+    MOCK_METHOD(void,
+                set_message_handler,
+                (const std::function<void(const std::string& message)>& handler),
+                (override));
+
+    MOCK_METHOD(void, set_allow_reconnecting, (bool allowed), (override));
+};
+
+typedef testing::NiceMock<MockWebsocketClient> DummyWebsocketClient;
+
+}
+
+#endif //ENJINSDK_MOCKWEBSOCKETCLIENT_HPP
