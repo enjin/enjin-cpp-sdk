1190cc0a059f4f4f5657a3a1ce07e4b7a866308d
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-03-16 19:34:24 -0700

    Added documentation for BaseSchema

--- include/enjinsdk/BaseSchema.hpp
+++ include/enjinsdk/BaseSchema.hpp
@@ -15,13 +15,16 @@
 
 namespace enjin::sdk {
 
-// TODO: Documentation
+/// \brief Base class for schema with functionality to send GraphQL requests to the platform and process the responses.
 class ENJINSDK_EXPORT BaseSchema {
 public:
     BaseSchema() = delete;
 
+    /// \brief Default destructor.
     ~BaseSchema() = default;
 
+    /// \brief Returns the logger for this schema.
+    /// \return The logger.
     [[nodiscard]] const std::shared_ptr<utils::Logger>& get_logger() const;
 
 protected:
@@ -29,15 +32,25 @@ protected:
     std::shared_ptr<utils::Logger> logger;
     std::string schema;
 
+    /// \brief The sole constructor for a base schema.
+    /// \param middleware The platform middleware.
+    /// \param schema The name of the schema.
+    /// \param logger The logger. Null pointer by default.
     BaseSchema(TrustedPlatformMiddleware middleware,
                std::string schema,
-               std::shared_ptr<utils::Logger> logger);
+               std::shared_ptr<utils::Logger> logger = nullptr);
 
+    /// \brief Creates the serialized request body to be sent to the platform.
+    /// \param request The request.
+    /// \return The serialized request body.
     std::string create_request_body(graphql::AbstractGraphqlRequest& request);
 
+    /// \brief Sends a request for one object from the platform.
+    /// \tparam T The type contained by the response.
+    /// \param request The request to being sent.
+    /// \return The future containing the response.
     template<class T>
     std::future<graphql::GraphqlResponse<T>> send_request_for_one(graphql::AbstractGraphqlRequest& request) {
-        // TODO: Placeholder section until function parameters are finalized.
         auto method = "POST";
         auto path = std::string("/graphql/").append(schema);
         auto content = http::IHttpClient::CONTENT_TYPE;
@@ -62,10 +75,13 @@ protected:
         });
     }
 
+    /// \brief Sends a request for many objects from the platform.
+    /// \tparam T The type contained the response's vector.
+    /// \param request The request to being sent.
+    /// \return The future containing the response.
     template<class T>
     std::future<graphql::GraphqlResponse<std::vector<T>>>
     send_request_for_many(graphql::AbstractGraphqlRequest& request) {
-        // TODO: Placeholder section until function parameters are finalized.
         auto method = "POST";
         auto path = std::string("/graphql/").append(schema);
         auto content = http::IHttpClient::CONTENT_TYPE;
