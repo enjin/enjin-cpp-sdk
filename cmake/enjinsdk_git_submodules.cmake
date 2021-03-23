4b7dfda6fe2600f9f2dfdfbff1814e05cffc1455
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-03-23 15:16:59 -0700

    Minor changes to CMake git submodule building file

--- cmake/enjinsdk_git_submodules.cmake
+++ cmake/enjinsdk_git_submodules.cmake
@@ -1,17 +1,18 @@
-macro(init_git_submodules)
-    find_package(Git
-            QUIET
-            REQUIRED)
+find_package(Git
+        QUIET
+        REQUIRED)
 
+macro(init_git_submodules)
     if (GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
         option(GIT_SUBMODULE "Check submodules during build" ON)
         if (GIT_SUBMODULE)
-            message(STATUS "Enjin: Submodule(s) updated")
             execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --remote
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     RESULT_VARIABLE GIT_SUBMOD_RESULT)
             if (NOT GIT_SUBMOD_RESULT EQUAL "0")
                 message(FATAL_ERROR "'git submodule update --init --remote' failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+            else ()
+                message(STATUS "Enjin: Submodule(s) updated")
             endif ()
         endif ()
     endif ()
