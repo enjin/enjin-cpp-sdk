fe57ca08f34334d0e639392ef3c65ff06923651e
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2021-04-15 19:43:07 -0700

    Moved Enjin hosts from their containing class to the 'enjin::sdk' namespace

--- include/enjinsdk/EnjinHosts.hpp
+++ include/enjinsdk/EnjinHosts.hpp
@@ -1,22 +1,16 @@
 #ifndef ENJINCPPSDK_ENJINHOSTS_HPP
 #define ENJINCPPSDK_ENJINHOSTS_HPP
 
-#include "enjinsdk_export.h"
-
 namespace enjin::sdk {
 
-/// \brief The networks hosts used by the Enjin Cloud.
-class ENJINSDK_EXPORT EnjinHosts {
-public:
-    /// \brief The URI for the kovan Enjin Cloud.
-    constexpr static char KOVAN[] = "https://kovan.cloud.enjin.io/";
+/// \brief The URI for the kovan Enjin Cloud.
+static constexpr char KOVAN[] = "https://kovan.cloud.enjin.io/";
 
-    /// \brief The URI for the main Enjin Cloud.
-    constexpr static char MAIN_NET[] = "https://cloud.enjin.io/";
+/// \brief The URI for the main Enjin Cloud.
+static constexpr char MAIN_NET[] = "https://cloud.enjin.io/";
 
-    /// \brief The URI for the JumpNet network.
-    constexpr static char JUMP_NET[] = "https://jumpnet.cloud.enjin.io/";
-};
+/// \brief The URI for the JumpNet network.
+static constexpr char JUMP_NET[] = "https://jumpnet.cloud.enjin.io/";
 
 }
 
