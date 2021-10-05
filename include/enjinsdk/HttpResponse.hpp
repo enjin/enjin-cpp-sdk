cda38baa365f7ac718e7f1170ae706e3c612a555
Bishop S Johnson <bishop@enjin.io>
2021-10-05 09:19:14 -0700

    Removed block-commented code from `HttpResponse`

--- include/enjinsdk/HttpResponse.hpp
+++ include/enjinsdk/HttpResponse.hpp
@@ -21,43 +21,7 @@
 #include <string>
 
 namespace enjin::sdk::http {
-/*
-class HttpResponse;
 
-/// \brief Builder class for HttpResponse class.
-class ENJINSDK_EXPORT HttpResponseBuilder {
-public:
-    /// \brief Default constructor.
-    HttpResponseBuilder() = default;
-
-    /// \brief Default destructor.
-    ~HttpResponseBuilder() = default;
-
-    /// \brief Builds the response.
-    /// \return The built response.
-    [[nodiscard]] HttpResponse build();
-
-    /// \brief Sets the status code the response will be built with.
-    /// \param code The status code.
-    /// \return This builder for chaining.
-    HttpResponseBuilder& code(unsigned short code);
-
-    /// \brief Sets the body data the response will be built with.
-    /// \param body The response body.
-    /// \return This builder for chaining.
-    HttpResponseBuilder& body(const std::string& body);
-
-    /// \brief Sets the content type header the response will be built with.
-    /// \param content_type The content type.
-    /// \return This builder for chaining.
-    HttpResponseBuilder& content_type(const std::string& content_type);
-
-private:
-    std::optional<unsigned short> m_code;
-    std::optional<std::string> m_body;
-    std::optional<std::string> m_content_type;
-};
-*/
 /// \brief Container class for a HTTP response.
 class ENJINSDK_EXPORT HttpResponse {
 public:
