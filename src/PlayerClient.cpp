d643934431781bd81955f667d5a1e745bf7de4a3
Bishop Johnson <bishop@enjin.io>
2021-10-12 13:44:51 -0700

    [SDK 82] Improved builder usages (#22)
    * Improved builder usage for `PlayerClient`
    * Improved builder usage for `ProjectClient`
    * Improved builder usage for `PusherEventService`
    * Improved builder usage for `HttpRequest`
    * Improved builder usage for `HttpResponse`
    * Added move-constructors to client and schema classes
    * Explicitly deleted copy-constructors from client and schema classes
    * Builders for `ProjectClient` and `PlayerClient` no longer return a unique-pointer from their `build()` member-function
    * PusherClient now only holds a reference to the websocket client
    * PusherEventService::Impl class now holds a unique-pointer for its websocket client instead of a shared-pointer
    * Added move-constructor to `PusherEventService`
    * Explicitly deleted copy-constructor from `PusherEventService`
    * Builder for `PusherEventService` no longer returns a unique-pointer from its `build()` member-function

--- src/PlayerClient.cpp
+++ src/PlayerClient.cpp
@@ -53,36 +53,42 @@ bool PlayerClient::is_closed() const {
     return !middleware.get_client()->is_open();
 }
 
-std::unique_ptr<PlayerClient> PlayerClientBuilder::build() {
+PlayerClient::PlayerClientBuilder PlayerClient::builder() {
+    return PlayerClient::PlayerClientBuilder();
+}
+
+PlayerClient PlayerClient::PlayerClientBuilder::build() {
     if (m_http_client == nullptr) {
 #if ENJINSDK_INCLUDE_HTTP_CLIENT_IMPL
         if (!m_base_uri.has_value()) {
             throw std::runtime_error("No base URI was set for default HTTP client implementation");
         }
 
-        TrustedPlatformMiddleware middleware(std::make_unique<http::HttpClient>(m_base_uri.value(),
-                                                                                    m_logger_provider));
-        return std::unique_ptr<PlayerClient>(new PlayerClient(std::move(middleware), m_logger_provider));
+        return PlayerClient(TrustedPlatformMiddleware(std::make_unique<http::HttpClient>(m_base_uri.value(),
+                                                                                         m_logger_provider)),
+                            m_logger_provider);
 #else
         throw std::runtime_error("Attempted building platform client without providing an HTTP client");
 #endif
     } else {
-        TrustedPlatformMiddleware middleware(std::move(m_http_client));
-        return std::unique_ptr<PlayerClient>(new PlayerClient(std::move(middleware), m_logger_provider));
+        return PlayerClient(TrustedPlatformMiddleware(std::move(m_http_client)),
+                            m_logger_provider);
     }
 }
 
-PlayerClientBuilder& PlayerClientBuilder::base_uri(const std::string& base_uri) {
+PlayerClient::PlayerClientBuilder& PlayerClient::PlayerClientBuilder::base_uri(const std::string& base_uri) {
     m_base_uri = base_uri;
     return *this;
 }
 
-PlayerClientBuilder& PlayerClientBuilder::http_client(std::unique_ptr<http::IHttpClient> http_client) {
+PlayerClient::PlayerClientBuilder&
+PlayerClient::PlayerClientBuilder::http_client(std::unique_ptr<http::IHttpClient> http_client) {
     m_http_client = std::move(http_client);
     return *this;
 }
 
-PlayerClientBuilder& PlayerClientBuilder::logger_provider(std::shared_ptr<utils::LoggerProvider> logger_provider) {
+PlayerClient::PlayerClientBuilder&
+PlayerClient::PlayerClientBuilder::logger_provider(std::shared_ptr<utils::LoggerProvider> logger_provider) {
     m_logger_provider = std::move(logger_provider);
     return *this;
 }
