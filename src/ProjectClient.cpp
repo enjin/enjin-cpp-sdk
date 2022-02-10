959007d7f396da675b5f89ac12cfb843932961f1
Bishop Johnson <BishopJohnson@users.noreply.github.com>
2022-02-09 20:14:18 -0800

    Automatic reauthentication for `ProjectClient` (#34)
    * Added `Timer` class to be used internally
    
    * Added overload for function in `RapidJsonUtils` to set a 64-bit integer (long)
    
    - Changed builders for `ProjectClient` and `PlayerClient` to return unique-pointers
    - Changed move constructors for `ProjectClient` and `PlayerClient` to be explicitly deleted
    - Added automatic reauthentication functionality to `ProjectClient`

--- src/ProjectClient.cpp
+++ src/ProjectClient.cpp
@@ -15,6 +15,7 @@
 
 #include "enjinsdk/ProjectClient.hpp"
 
+#include <chrono>
 #include <stdexcept>
 #include <utility>
 
@@ -30,8 +31,15 @@
 namespace enjin::sdk {
 
 ProjectClient::ProjectClient(TrustedPlatformMiddleware middleware,
-                             std::shared_ptr<utils::LoggerProvider> logger_provider)
-        : ProjectSchema(std::move(middleware), std::move(logger_provider)) {
+                             bool automatic_reauthentication,
+                             std::shared_ptr<utils::LoggerProvider> logger_provider,
+                             std::optional<std::function<void()>> reauthentication_stopped_handler)
+        : ProjectSchema(std::move(middleware), std::move(logger_provider)),
+          automatic_reauthentication_enabled(automatic_reauthentication),
+          reauthentication_stopped_handler(std::move(reauthentication_stopped_handler)) {
+    if (automatic_reauthentication) {
+        auth_timer = std::make_unique<utils::Timer>();
+    }
 }
 
 ProjectClient::~ProjectClient() {
@@ -39,10 +47,42 @@ ProjectClient::~ProjectClient() {
 }
 
 void ProjectClient::auth(std::string token) {
-    middleware.get_handler()->set_auth_token(token);
+    auth(token, std::nullopt);
+}
+
+void ProjectClient::auth(models::AccessToken access_token) {
+    const auto& token = access_token.get_token();
+    const auto& expires_in = access_token.get_expires_in();
+    if (token.has_value() ^ expires_in.has_value()) {
+        throw std::runtime_error("Access token has partial data.");
+    }
+
+    auth(token, expires_in);
+}
+
+std::future<void> ProjectClient::auth_client(std::string uuid, std::string secret) {
+    if (is_closed()) {
+        throw std::runtime_error("Cannot authenticate after client was closed.");
+    }
+
+    if (is_automatic_reauthentication_enabled()) {
+        std::unique_lock<std::mutex> lock(auth_mutex);
+        ProjectClient::uuid = uuid;
+        ProjectClient::secret = secret;
+        lock.unlock();
+    }
+
+    return send_request_and_auth(uuid, secret);
 }
 
 void ProjectClient::close() {
+    std::unique_lock<std::mutex> lock(auth_mutex);
+    if (auth_timer != nullptr) {
+        auth_timer->cancel();
+        reauthentication_running = false;
+    }
+    lock.unlock();
+
     middleware.close();
 }
 
@@ -50,15 +90,95 @@ bool ProjectClient::is_authenticated() const {
     return middleware.get_handler()->is_authenticated();
 }
 
+bool ProjectClient::is_automatic_reauthentication_enabled() const {
+    return automatic_reauthentication_enabled;
+}
+
 bool ProjectClient::is_closed() const {
     return middleware.is_closed();
+}
+
+bool ProjectClient::is_reauthentication_running() const {
+    std::lock_guard<std::mutex> guard(auth_mutex);
+    return reauthentication_running;
 }
 
 ProjectClient::ProjectClientBuilder ProjectClient::builder() {
     return {};
 }
 
-ProjectClient ProjectClient::ProjectClientBuilder::build() {
+void ProjectClient::auth(std::optional<std::string> token, std::optional<long> expires_in) {
+    bool timer_restarted = false;
+    std::unique_lock<std::mutex> lock(auth_mutex);
+    if (is_automatic_reauthentication_enabled() && uuid.has_value() && secret.has_value()) {
+        timer_restarted = restart_authentication_timer(expires_in);
+    }
+    lock.unlock();
+
+    middleware.get_handler()->set_auth_token(token.value_or(""));
+
+    if (reauthentication_stopped_handler.has_value() && auth_timer != nullptr && !timer_restarted) {
+        reauthentication_stopped_handler.value()();
+    }
+}
+
+bool ProjectClient::restart_authentication_timer(std::optional<long> expires_in) {
+    auth_timer->cancel();
+    reauthentication_running = false;
+
+    if (!expires_in.has_value() || expires_in.value() <= 0) {
+        return false;
+    }
+
+    auto duration = expires_in.value();
+    if (duration - PREEMPT_AUTH_EXPIRATION_TIME > 0) {
+        duration -= PREEMPT_AUTH_EXPIRATION_TIME;
+    }
+
+    auth_timer->schedule([this]() {
+        send_request_and_auth();
+    }, std::chrono::milliseconds(duration * 1000));
+    reauthentication_running = true;
+
+    return true;
+}
+
+std::future<void> ProjectClient::send_request_and_auth(std::string uuid, std::string secret) {
+    return std::async([this, uuid = std::move(uuid), secret = std::move(secret)]() {
+        auto req = project::AuthProject()
+                .set_uuid(uuid)
+                .set_secret(secret);
+        try {
+            auto res = auth_project(req).get();
+            if (res.is_successful()) {
+                auth(res.get_result().value());
+            } else {
+                auth(std::nullopt, std::nullopt);
+            }
+        } catch (const std::exception& e) {
+            if (logger_provider != nullptr) {
+                logger_provider->log(utils::LogLevel::ERR, "Automatic AuthProject request failed.", e);
+            }
+
+            throw e;
+        }
+    });
+}
+
+void ProjectClient::send_request_and_auth() {
+    std::unique_lock<std::mutex> lock(auth_mutex);
+    if (!uuid.has_value() || !secret.has_value()) {
+        throw std::runtime_error("UUID or secret are not set while attempting reauthentication.");
+    }
+
+    std::string uuid = ProjectClient::uuid.value();
+    std::string secret = ProjectClient::secret.value();
+    lock.unlock();
+
+    send_request_and_auth(uuid, secret);
+}
+
+std::unique_ptr<ProjectClient> ProjectClient::ProjectClientBuilder::build() {
     if (m_http_client == nullptr) {
 #if ENJINSDK_INCLUDE_HTTP_CLIENT_IMPL
         if (!m_base_uri.has_value()) {
@@ -71,17 +191,28 @@ ProjectClient ProjectClient::ProjectClientBuilder::build() {
             client->set_logger(log_level, m_logger_provider);
         }
 
-        return {TrustedPlatformMiddleware(std::move(client)), m_logger_provider};
+        return std::unique_ptr<ProjectClient>(new ProjectClient(TrustedPlatformMiddleware(std::move(client)),
+                                                                m_automatic_reauthentication.value_or(false),
+                                                                m_logger_provider,
+                                                                std::move(m_reauthentication_stopped_handler)));
 #else
         throw std::runtime_error("Attempted building platform client without providing an HTTP client");
 #endif
     } else {
-        return {TrustedPlatformMiddleware(std::move(m_http_client)), m_logger_provider};
+        return std::unique_ptr<ProjectClient>(new ProjectClient(TrustedPlatformMiddleware(std::move(m_http_client)),
+                                                                m_automatic_reauthentication.value_or(false),
+                                                                m_logger_provider,
+                                                                std::move(m_reauthentication_stopped_handler)));
     }
 }
 
 ProjectClient::ProjectClientBuilder& ProjectClient::ProjectClientBuilder::base_uri(std::string base_uri) {
     m_base_uri = std::move(base_uri);
+    return *this;
+}
+
+ProjectClient::ProjectClientBuilder& ProjectClient::ProjectClientBuilder::enable_automatic_reauthentication() {
+    m_automatic_reauthentication = true;
     return *this;
 }
 
@@ -100,6 +231,12 @@ ProjectClient::ProjectClientBuilder::http_log_level(http::HttpLogLevel http_log_
 ProjectClient::ProjectClientBuilder&
 ProjectClient::ProjectClientBuilder::logger_provider(std::shared_ptr<utils::LoggerProvider> logger_provider) {
     m_logger_provider = std::move(logger_provider);
+    return *this;
+}
+
+ProjectClient::ProjectClientBuilder&
+ProjectClient::ProjectClientBuilder::reauthentication_stopped_handler(std::function<void()> handler) {
+    m_reauthentication_stopped_handler = std::move(handler);
     return *this;
 }
 
---
+++ test/unit/ProjectClientReauthenticateTest.cpp
@@ -0,0 +1,253 @@
+/* Copyright 2021 Enjin Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "HttpClient.hpp"
+#include "MockHttpServer.hpp"
+#include "RapidJsonUtils.hpp"
+#include "enjinsdk/HttpHeaders.hpp"
+#include "enjinsdk/ProjectClient.hpp"
+#include "enjinsdk/models/AccessToken.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "rapidjson/document.h"
+#include <chrono>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <thread>
+
+using enjin::sdk::models::AccessToken;
+using testing::MockFunction;
+using testing::NiceMock;
+using namespace enjin::sdk;
+using namespace enjin::sdk::http;
+using namespace enjin::sdk::utils;
+using namespace enjin::test::mocks;
+
+class ProjectClientReauthenticateTestSuite {
+public:
+    static constexpr char FAKE_UUID[] = "Fake Uuid";
+    static constexpr char FAKE_SECRET[] = "Fake Secret";
+    static constexpr char JSON[] = "application/json; charset=utf-8";
+
+    static AccessToken create_fake_access_token(const std::string& token, long expires_in) {
+        auto access_token = AccessToken();
+        auto ss = std::stringstream();
+
+        ss << R"({"accessToken":")" << token << R"(","expiresIn":)" << expires_in << "}";
+        access_token.deserialize(ss.str());
+
+        return access_token;
+    }
+
+    static std::string serialize_token(const AccessToken& access_token) {
+        rapidjson::Document document(rapidjson::kObjectType);
+
+        const auto& token = access_token.get_token();
+        if (token.has_value()) {
+            set_string_member(document, "accessToken", token.value());
+        }
+        const auto& expires_in = access_token.get_expires_in();
+        if (expires_in.has_value()) {
+            set_integer_member(document, "expiresIn", expires_in.value());
+        }
+
+        return document_to_string(document);
+    }
+};
+
+class ProjectClientReauthenticateTest : public ProjectClientReauthenticateTestSuite,
+                                        public testing::Test {
+public:
+    std::unique_ptr<ProjectClient> class_under_test;
+
+    NiceMock<MockFunction<void()>> mock_reauthentication_stopped_handler;
+
+    MockHttpServer mock_server;
+
+protected:
+    void SetUp() override {
+        mock_server.start();
+        class_under_test = ProjectClient::builder()
+                .http_client(std::make_unique<HttpClient>(mock_server.uri()))
+                .enable_automatic_reauthentication()
+                .reauthentication_stopped_handler(mock_reauthentication_stopped_handler.AsStdFunction())
+                .build();
+    }
+
+    void TearDown() override {
+        class_under_test.reset();
+        mock_server.stop();
+    }
+};
+
+class ProjectClientReauthenticateTestInvalidDurations : public ProjectClientReauthenticateTestSuite,
+                                                        public testing::TestWithParam<long> {
+public:
+    std::unique_ptr<ProjectClient> class_under_test;
+
+    NiceMock<MockFunction<void()>> mock_reauthentication_stopped_handler;
+
+    MockHttpServer mock_server;
+
+protected:
+    void SetUp() override {
+        mock_server.start();
+        class_under_test = ProjectClient::builder()
+                .http_client(std::make_unique<HttpClient>(mock_server.uri()))
+                .enable_automatic_reauthentication()
+                .reauthentication_stopped_handler(mock_reauthentication_stopped_handler.AsStdFunction())
+                .build();
+    }
+
+    void TearDown() override {
+        class_under_test.reset();
+        mock_server.stop();
+    }
+};
+
+TEST_F(ProjectClientReauthenticateTest, AuthWhileUuidAndSecretAreEmptyDoesNotStartReauthenticationTimer) {
+    // Arrange
+    const AccessToken fakeToken = create_fake_access_token("xyz", 1000L);
+
+    // Assumption
+    ASSERT_TRUE(class_under_test->is_automatic_reauthentication_enabled()) << "Automatic reauthentication is disabled.";
+    ASSERT_FALSE(class_under_test->is_reauthentication_running()) << "Timer is running before call to authenticate.";
+
+    // Act
+    class_under_test->auth(fakeToken);
+
+    // Assert
+    ASSERT_FALSE(class_under_test->is_reauthentication_running()) << "Timer is running after call to authenticate.";
+}
+
+TEST_F(ProjectClientReauthenticateTest, AuthProjectGivenValidUuidAndSecretClientReauthenticates) {
+    // Arrange - Data
+    const std::string auth_token("FakeAuthToken");
+    const std::string expected_path = "/graphql/project";
+    const std::string expected_header = AUTHORIZATION;
+    const std::string expected_header_value = "Bearer " + auth_token;
+    const Request request = Request::create().with_path(expected_path).using_post();
+    const AccessToken fake_token = create_fake_access_token(auth_token, 1);
+    const std::string response_body = R"({"data":{"result":)" + serialize_token(fake_token) + "}}";
+
+    // Arrange - Mocking
+    mock_server.given(request)
+               .respond_with(Response::create()
+                                     .with_success()
+                                     .with_header(CONTENT_TYPE, JSON)
+                                     .with_body(response_body));
+
+    // Assumption
+    ASSERT_TRUE(class_under_test->is_automatic_reauthentication_enabled()) << "Automatic reauthentication is disabled.";
+    ASSERT_FALSE(class_under_test->is_authenticated()) << "Is authenticated before request is sent.";
+
+    // Act
+    auto future = class_under_test->auth_client(FAKE_UUID, FAKE_SECRET);
+
+    future.wait_for(std::chrono::milliseconds(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    // Assert
+    ASSERT_NO_THROW(future.get()) << "AuthClient operation resulted in an error.";
+    ASSERT_TRUE(class_under_test->is_authenticated()) << "Client was not authenticated.";
+
+    // Verify
+    auto log_entries = mock_server.find_received_requests(request);
+    ASSERT_GT(log_entries.size(), 1) << "Did not receive more than one request.";
+
+    const auto& req1 = log_entries[0].get_request_message().value();
+    const auto& req1_headers = req1.get_headers();
+    ASSERT_EQ(req1.get_path(), expected_path) << "Req1 is not to the expected path.";
+    ASSERT_FALSE(req1_headers.find(expected_header) != req1_headers.end()) << "Req1 had authorization.";
+
+    const auto& req2 = log_entries[1].get_request_message().value();
+    const auto& req2_headers = req2.get_headers();
+    ASSERT_EQ(req2.get_path(), expected_path) << "Req2 is not to the expected path.";
+    ASSERT_EQ(req2_headers.at(expected_header), expected_header_value) << "Req2 did not have authorization.";
+}
+
+TEST_F(ProjectClientReauthenticateTest, AuthGivenEmptyTokenStopsReauthenticationTimer) {
+    // Arrange - Data
+    const AccessToken empty_token;
+    const AccessToken fake_token = create_fake_access_token("xyz", 1000L);
+    const std::string response_body = R"({"data":{"result":)" + serialize_token(fake_token) + "}}";
+
+    // Arrange - Expectations
+    EXPECT_CALL(mock_reauthentication_stopped_handler, Call()).Times(1);
+
+    // Arrange - Stubbing
+    mock_server.given(Request::create()
+                              .with_path("/graphql/project")
+                              .using_post())
+               .respond_with(Response::create()
+                                     .with_success()
+                                     .with_header(CONTENT_TYPE, JSON)
+                                     .with_body(response_body));
+
+    // Arrange - Data (continued)
+    class_under_test->auth_client(FAKE_UUID, FAKE_SECRET).wait_for(std::chrono::milliseconds(1000));
+
+    // Assumption
+    ASSERT_TRUE(class_under_test->is_authenticated()) << "Is not authenticated.";
+    ASSERT_TRUE(class_under_test->is_reauthentication_running()) << "Reauthentication is not running.";
+
+    // Act
+    class_under_test->auth(empty_token);
+
+    // Assert
+    ASSERT_FALSE(class_under_test->is_reauthentication_running()) << "Reauthentication is still running.";
+
+    // Verify (done by GTest)
+}
+
+TEST_P(ProjectClientReauthenticateTestInvalidDurations, AuthGivenInvalidDurationStopsReauthenticationTimer) {
+    // Arrange - Data
+    const long expires_in = GetParam();
+    const AccessToken bad_token = create_fake_access_token("xyz", expires_in);
+    const AccessToken fake_token = create_fake_access_token("xyz", 1000L);
+    const std::string response_body = R"({"data":{"result":)" + serialize_token(fake_token) + "}}";
+
+    // Arrange - Stubbing
+    mock_server.given(Request::create()
+                              .with_path("/graphql/project")
+                              .using_post())
+               .respond_with(Response::create()
+                                     .with_success()
+                                     .with_header(CONTENT_TYPE, JSON)
+                                     .with_body(response_body));
+
+    // Arrange - Expectations
+    EXPECT_CALL(mock_reauthentication_stopped_handler, Call()).Times(1);
+
+    // Arrange - Data (continued)
+    class_under_test->auth_client(FAKE_UUID, FAKE_SECRET).wait_for(std::chrono::milliseconds(1000));
+
+    // Assumption
+    ASSERT_TRUE(class_under_test->is_authenticated()) << "Is not authenticated.";
+    ASSERT_TRUE(class_under_test->is_reauthentication_running()) << "Reauthentication is not running.";
+
+    // Act
+    class_under_test->auth(bad_token);
+
+    // Assert
+    ASSERT_FALSE(class_under_test->is_reauthentication_running()) << "Reauthentication is still running.";
+
+    // Verify (done by GTest)
+}
+
+INSTANTIATE_TEST_SUITE_P(InvalidDurations,
+                         ProjectClientReauthenticateTestInvalidDurations,
+                         testing::Values(0L, -1L, -100L));
